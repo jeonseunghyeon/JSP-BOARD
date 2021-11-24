604b0795dca08021e6794de33e8c66008df5574a
jeonseunghyeon <klzxklzx123@naver.com>
2021-11-25 01:41:59 +0900

    dao

--- src/sec/ex/UserDAO.java
+++ src/sec/ex/UserDAO.java
@@ -1,5 +1,40 @@
 package sec.ex;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserDAO {
+	
+	private Connection conn;
+	private ResultSet rs;
+	private PreparedStatement pstmt;
+	
+	public UserDAO() {
+		
+		
+		try {
+			
+			String URL ="jdbc:mysql://localhost:3306/jsp_web_commu";
+			String id = "root";
+			String password ="chun3032";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(URL,id,password);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	public int login(String u_id, String u_password) {
+		String sql ="select u_Password from";
+		
+		
+		
+		
+		
+		return -2;
+	}
 
 }
