4bdb30da3c990c7efaf9ad29c3a48eb353d1c977
dlwndrhs05 <dlwndrhs05@gmail.com>
2021-11-25 01:01:38 +0900

    basic

--- WebContent/sql/Mysql.sql
+++ WebContent/sql/Mysql.sql
@@ -1,7 +1,8 @@
 CREATE SCHEMA `jsp_web_commu` 
 CREATE TABLE `jsp_web_commu`.`member` (
-  `MemberNUM` INT NOT NULL,
-  `MemberID` VARCHAR(10) NOT NULL,
-  `MemberPassword` VARCHAR(12) NOT NULL,
-  PRIMARY KEY (`MemberNUM`),
-  UNIQUE INDEX `MemberID_UNIQUE` (`MemberID` ASC) VISIBLE);
+  `u_NUM` INT NOT NULL,
+  `u_ID` VARCHAR(10) NOT NULL,
+  `u_Password` VARCHAR(12) NOT NULL,
+  `u_name` VARCHAR(10) NOT NULL
+  PRIMARY KEY (`u_NUM`),
+  UNIQUE INDEX `u_ID_UNIQUE` (`u_ID` ASC),(`u_Name` ASC)VISIBLE);
